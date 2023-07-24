0c9a7ddb80291062251f33f2c97721a6a4d74065
Joseph Hendrix <joeylhendrix@gmail.com>
2023-07-24 05:49:38 -0500

    test panics

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,11 @@
 use lee_alg::*;
 
 #[test]
+#[should_panic]
+fn bad_matrix() {
+    Matrix::new(vec![vec![1, 2, 3], vec![4, 5]]);
+}
+#[test]
 fn square() {
     let tester = Matrix::new(vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
     let test_notsq = Matrix::new(vec![
@@ -38,10 +43,17 @@ fn dot_prod() {
     assert_eq!(78, col1.dot(&col2));
 }
 
+#[test]
+#[should_panic]
+fn bad_dot_prod() {
+    let col1 = Column::new(vec![1, 2, 3]);
+    let col2 = Column::new(vec![1, 2]);
+    col1.dot(&col2);
+}
 
 #[test]
 fn transpose() {
     let a = Matrix::new(vec![vec![1, 2, 3], vec![4, 5, 6]]);
-    let b = Matrix::new(vec![vec![1, 4], vec![2, 5], vec![ 3, 6]]);
+    let b = Matrix::new(vec![vec![1, 4], vec![2, 5], vec![3, 6]]);
     assert_eq!(a.transpose(), b);
 }
